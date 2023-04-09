b7a6a60628e169fd9bbbc662125e353461b5f4de
ygqygq2 <ygqygq2@qq.com>
2023-04-09 23:53:26 +0800

    feat: 增加子组件
    Co-authored-by: ygqygq2 <29ygq@sina.com>

---
+++ src/app/components/Layout/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const Layout: NextPage = () => {
+  return (
+    <div>
+      <main>layout</main>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/app/components/Navbar/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next';
+
+const Navbar: NextPage = () => {
+  return <div>导航</div>;
+};
+
+export default Navbar;
