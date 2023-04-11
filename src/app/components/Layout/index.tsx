7fb216c9a2a124d2ee30f990332b326e0d7a4f7b
ygqygq2 <ygqygq2@qq.com>
2023-04-12 00:29:09 +0800

    feat: 导航样式修改
    Co-authored-by: ygqygq2 <29ygq@sina.com>

--- src/app/components/Layout/index.tsx
+++ src/app/components/Layout/index.tsx
@@ -4,21 +4,19 @@ import type { NextPage } from 'next';
 
 import Link from 'next/link';
 
-import { useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 
 import { navs } from './config';
 import styles from './index.module.scss';
 
 const Layout: NextPage = () => {
-  const router = useRouter();
-  console.log(router);
-  // const {pathname} = useRouter();
+  const pathname = usePathname();
   return (
     <div className={styles.navbar}>
       <section className={styles.logoArea}>Blog-C</section>
       <section className={styles.linkArea}>
         {navs?.map((nav) => (
-          <Link className={nav?.value === 'pathname' ? styles.active : ''} key={nav?.label} href={nav?.value}>
+          <Link className={nav?.value === pathname ? styles.active : ''} key={nav?.label} href={nav?.value}>
             {nav?.label}
           </Link>
         ))}
